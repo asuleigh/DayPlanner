89aacb7d392c9d42b1ef07906c67dbcade524579
Amanda Surber <surberaleigh@gmail.com>
2019-10-27 22:38:52 -0400

    updating layout and buttons for index.html, renaming ids for css, clearing incorrect js

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,9 +1,4 @@
-var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
+//Hints:
+//var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
 
-var currentHour = new Date().getHours();//Current hour in military time
-
-function currentDateTime() {
-    var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
-    document.getElementById("currentDate") = currentDateAndTime.getDate();
-    
-}
+//var currentHour = new Date().getHours();//Current hour in military time
