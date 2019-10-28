343004432e91e76f7a10fb298b7d52010e59d5d7
Amanda Surber <surberaleigh@gmail.com>
2019-10-28 07:11:59 -0400

    Added time placeholders in index.html. Added comments on progress in main.js

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,3 +1,9 @@
+//Still working on functionality. Will ask to be reviewed again once js is finished
+
+//var dateNow = new Date();
+//var dateNow = document.getElementsByClassName("currentDate") = Date();
+
+
 //Hints:
 //var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
 
