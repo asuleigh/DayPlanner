03b24b1caea603b09de33ab0cc797305b4fdf3d5
Amanda Surber <surberaleigh@gmail.com>
2019-12-01 19:32:53 -0500

    changed input field type, added js to allow for saving and retrieving input from localStorage

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,10 +1,24 @@
-//Still working on functionality. Will ask to be reviewed again once js is finished
+// Function for setting day and month at top of page using Moment
+$('#currentDate').text(moment().format('dddd, MMMM Do'));
 
-//var dateNow = new Date();
-//var dateNow = document.getElementsByClassName("currentDate") = Date();
+/* Setting up function to get values from localStorage using ids from 
+the input areas in the HTML*/
+
+$('#9am').val(localStorage.getItem('#9am'));
+$('#10am').val(localStorage.getItem('#10am'));
+$('#11am').val(localStorage.getItem('#11am'));
+$('#12pm').val(localStorage.getItem('#12pm'));
+$('#1pm').val(localStorage.getItem('#1pm'));
+$('#2pm').val(localStorage.getItem('#2pm'));
+$('#3pm').val(localStorage.getItem('#3pm'));
+$('#4pm').val(localStorage.getItem('#4pm'));
+$('#5pm').val(localStorage.getItem('#5pm'));
+
+/* Function for storing input upon clicking the save button using the 
+(this) method */
+$('.btn').click( function() {
+    let storeInput = $(this).val();
+    localStorage.setItem(storeInput, $(storeInput).val());
+    });
 
 
-//Hints:
-//var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)
-
-//var currentHour = new Date().getHours();//Current hour in military time
