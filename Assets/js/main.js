f30fb98b87031f2479afbcedab9a650b06f7ee56
Amanda Surber <surberaleigh@gmail.com>
2019-12-02 00:07:23 -0500

    fixed localStorage working with background colors. Moved localStorage script below background color script

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,25 +1,6 @@
 // Function for setting day and month at top of page using Moment
 $('#currentDate').text(moment().format('dddd, MMMM Do'));
 
-/* Setting up function to get values from localStorage using ids from 
-the input areas in the HTML*/
-$('#9am').val(localStorage.getItem('#9am'));
-$('#10am').val(localStorage.getItem('#10am'));
-$('#11am').val(localStorage.getItem('#11am'));
-$('#12pm').val(localStorage.getItem('#12pm'));
-$('#1pm').val(localStorage.getItem('#1pm'));
-$('#2pm').val(localStorage.getItem('#2pm'));
-$('#3pm').val(localStorage.getItem('#3pm'));
-$('#4pm').val(localStorage.getItem('#4pm'));
-$('#5pm').val(localStorage.getItem('#5pm'));
-
-/* Function for storing input upon clicking the save button using the 
-(this) method */
-$('.btn').click( function() {
-    let storeInput = $(this).val();
-    localStorage.setItem(storeInput, $(storeInput).val());
-    });
-
 /* Create a function for displaying colors inside of the input sections according
    to time of day*/
 function display() {
@@ -56,5 +37,22 @@ function display() {
 // Display function for input colors
 display();
 
+/* Setting up function to get values from localStorage using ids from 
+the input areas in the HTML*/
+$('#9am').val(localStorage.getItem('#9am'));
+$('#10am').val(localStorage.getItem('#10am'));
+$('#11am').val(localStorage.getItem('#11am'));
+$('#12pm').val(localStorage.getItem('#12pm'));
+$('#1pm').val(localStorage.getItem('#1pm'));
+$('#2pm').val(localStorage.getItem('#2pm'));
+$('#3pm').val(localStorage.getItem('#3pm'));
+$('#4pm').val(localStorage.getItem('#4pm'));
+$('#5pm').val(localStorage.getItem('#5pm'));
 
+/* Function for storing input upon clicking the save button using the 
+(this) method */
+$('.btn').click( function() {
+    let storeInput = $(this).val();
+    localStorage.setItem(storeInput, $(storeInput).val());
+    });
 
